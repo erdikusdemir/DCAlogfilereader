dd2de0dc28c5e88d563697b1506568ef08e01c73
Erdi Kuşdemir <39906464+erdikusdemir@users.noreply.github.com>
2019-09-09 10:19:28 +0200

    Add files via upload

---
+++ source codes/M1 DCA log file opener/WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.zmn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Load.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Load.Location = new System.Drawing.Point(775, 12);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(838, 512);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // zmn
+            // 
+            this.zmn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.zmn.Location = new System.Drawing.Point(12, 13);
+            this.zmn.Name = "zmn";
+            this.zmn.ReadOnly = true;
+            this.zmn.Size = new System.Drawing.Size(162, 20);
+            this.zmn.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            this.label1.UseWaitCursor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zmn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Load);
+            this.Name = "main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "M1 DCA log file reader";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox zmn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ source codes/M1 DCA log file opener/WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.InitialDirectory = "C:\\DCA\\Logfiles";
+            // dosya.Filter = "*.log";
+
+            if (dosya.ShowDialog() != DialogResult.Cancel)
+            {
+
+                string sLine = "";
+
+                try
+                {
+                    //Pass the file you selected with the OpenFileDialog control to
+                    //the StreamReader Constructor.
+                    System.IO.StreamReader FileStream = new System.IO.StreamReader(dosya.FileName);
+                    //You must set the value to false when you are programatically adding rows to
+                    //a DataGridView.  If you need to allow the user to add rows, you
+                    //can set the value back to true after you have populated the DataGridView
+                    dataGridView1.AllowUserToAddRows = false;
+
+
+
+                    //Read the first line of the text file
+                    string zaman = FileStream.ReadLine();
+                    zaman = FileStream.ReadLine();
+                    zaman = FileStream.ReadLine();
+                    zmn.Text = zaman;
+                    sLine = FileStream.ReadLine();
+                    //The Split Command splits a string into an array, based on the delimiter you pass.
+                    //I chose to use a semi-colon for the text delimiter.
+                    //Any character can be used as a delimeter in the split command.
+                    string[] s = sLine.Split('\t');
+
+
+
+
+                    //In this example, I placed the field names in the first row.
+                    //The for loop below is used to create the columns and use the text values in
+                    //the first row for the column headings.
+                    for (int i = 0; i <= s.Count() - 1; i++)
+                    {
+                        DataGridViewColumn colHold = new DataGridViewTextBoxColumn();
+                        colHold.Name = "col" + System.Convert.ToString(i);
+                        colHold.HeaderText = s[i].ToString();
+                        colHold.SortMode = DataGridViewColumnSortMode.NotSortable;
+                        dataGridView1.Columns.Add(colHold);
+                    }
+
+                    //Read the next line in the text file in order to pass it to the
+                    //while loop below
+                    sLine = FileStream.ReadLine();
+                    //The while loop reads each line of text.
+                    while (sLine != null)
+                    {
+                        //Adds a new row to the DataGridView for each line of text.
+                        dataGridView1.Rows.Add();
+
+                        //This for loop loops through the array in order to retrieve each
+                        //line of text.
+                        for (int i = 0; i <= s.Count() - 1; i++)
+                        {
+                            //Splits each line in the text file into a string array
+                            s = sLine.Split('\t');
+                            //Sets the value of the cell to the value of the text retreived from the text file.
+                            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[i].Value = s[i].ToString();
+                        }
+                        sLine = FileStream.ReadLine();
+                    }
+                    //Close the selected text file.
+                    FileStream.Close();
+
+                }
+                catch (Exception err)
+                {
+                    //Display any errors in a Message Box.
+                    MessageBox.Show("Error" + err.Message, "Program Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }//if
+        }//load.click
+
+
+    }
+}
